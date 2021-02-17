3fff769810c0cbdd6c0e3dfcc17bdc7a939bb5a0
Joel Ortiz <joelandapophis@gmail.com>
2021-02-16 21:08:01 -0600

    typingdna

---
+++ src/controllers/TypingDNAController.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { paths } from "../config/Config";
+import { typingDNAVerify, typingDNAGetUser, typingDNADeleteUser } from "../services/TypingDNAService";
+
+export default function TypingDNAController(app: express.Application){
+
+    app.post(paths.TYPING_DNA_VERIFY, typingDNAVerify);
+
+    app.get(paths.TYPING_DNA_GET_USER, typingDNAGetUser);
+
+    app.delete(paths.TYPING_DNA_DELETE_USER, typingDNADeleteUser);
+
+}
